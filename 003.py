ece7dc0f948384e47062341bcf222bd18163fadb
Nishant <si6xdx@gmail.com>
2023-08-25 01:42:35 -0400

    Add number

---
+++ 003.py
@@ -0,0 +1,9 @@
+number = input("Give me a number :-")
+#print(type(number))
+
+number1 = int(number[0])
+number2 = int(number[1])
+
+result = number1 + number2
+
+print(f"The sum of number :- {number} is {result}")
